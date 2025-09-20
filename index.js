e6558a5a45c8979c2831e4b0cabc4d7a0c9bd61f
alireza akbarzadeh <akbarzadeh.ali@tapsi.shop>
2025-09-20 09:34:54 +0330

    feat: add ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hellow DevOps');
+console.log('Im Leaning CI/CD using github actions');
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Starting Testing...');
+setTimeout(() => {
+  console.log('Wating 3 seconds...');
+}, 3000);
+console.log('Finishing tests...');
